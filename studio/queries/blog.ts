f71c266e994e5732f0978b9cf0a9ac116548d503
HaakonSvane <haakonsvane@gmail.com>
2024-12-22 21:10:16 +0000

    export studio queries, type and page

--- studio/queries/blog.ts
+++ studio/queries/blog.ts
@@ -1,11 +1,9 @@
-import {defineQuery} from 'groq'
+import { defineQuery } from "groq";
 
-export const getBlogPostsQuery = () =>
-  defineQuery(
-    `*[_type == 'blogPost'] | order(_createdAt desc) {title, synopsis, slug, 'image': image.asset->url`,
-  )
+export const getBlogPostsQuery = defineQuery(
+    `*[_type == 'blogPost'] | order(_createdAt desc) {title, synopsis, slug, 'image': image.asset->}`,
+);
 
-export const getBlogPostQuery = (blogPostSlug: string) =>
-  defineQuery(
-    `*[_type == 'blogPost' && slug.current == ${blogPostSlug}][0] | {title, synopsis, body, 'image': image.asset->url}`,
-  )
+export const getBlogPostQuery = defineQuery(
+    `*[_type == 'blogPost' && slug.current == $blogPostSlug][0] | {title, synopsis, body, 'image': image.asset->}`,
+);
